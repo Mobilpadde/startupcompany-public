6816ae50c78a2767caf16760d5a4499c0f9b4f63
Jonas Hovgaard <j@jhovgaard.dk>
2015-11-05 16:59:00 +0100

    Create marketingOptions.js

---
+++ marketingOptions.js
@@ -0,0 +1,16 @@
+[
+    {
+      id: 1,
+      name: 'Banner Ads',
+      description: 'Shows your banner on various unspecific websites. Impressions are cheaper, but generates fewer new users per impression.',
+      pricePerImpression: 0.005,
+      conversationRate: 0.2
+    },
+    {
+      id: 2,
+      name: 'Targeted Banner Ads',
+      description: 'Shows your banner websites related to your product. Impressions are more expensive, but generates more users per impression.',
+      pricePerImpression: 0.009,
+      conversationRate: 0.6
+    }  
+]
