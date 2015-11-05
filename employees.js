d8b831a11e06ba160fa7fdb479036ec96dc15f4a
Jonas Hovgaard <j@jhovgaard.dk>
2015-11-05 16:52:18 +0100

    Create employees.js

---
+++ employees.js
@@ -0,0 +1,21 @@
+[
+    // Backend Developers
+    { id: 1, name: 'Gage Kingston', experience: 281600, skill: 50, type: EmployeeTypes.BackendDeveloper, image: 'images/avatars/male/1.png', salary: 6500, skills:[0,9], avatar: 'male1', progress: 0, happiness: 50 },
+    { id: 2, name: 'Ricki Payton', experience: 70400, skill: 30, type: EmployeeTypes.BackendDeveloper, image: 'images/avatars/male/2.png', salary: 3200, skills:[0,9], avatar: 'male1', progress: 0, happiness: 50 },
+    { id: 3, name: 'Abby Jensen', experience: 211200, skill: 50, type: EmployeeTypes.BackendDeveloper, image: 'images/avatars/female/1.png', salary: 5000, skills:[0,9], avatar: 'female1', progress: 0, happiness: 50 },
+
+    // Frontend Developers
+    { id: 4, name: 'Trish Piperk', experience: 162500, skill: 40, type: EmployeeTypes.FrontendDeveloper, image: 'images/avatars/female/2.png', salary: 3700, skills:[0,1], avatar: 'female1', progress: 0, happiness: 50 },
+    { id: 5, name: 'Lacy Baz', experience: 70400, skill: 30, type: EmployeeTypes.FrontendDeveloper, image: 'images/avatars/male/3.png', salary: 3000, skills:[0,1], avatar: 'male1', progress: 0, happiness: 50 },
+    { id: 6, name: 'Debra Elissa', experience: 246400, skill: 50, type: EmployeeTypes.FrontendDeveloper, image: 'images/avatars/female/3.png', salary: 5000, skills:[0,1], avatar: 'female1', progress: 0, happiness: 50 },
+    
+    // Customer Service Agents
+    { id: 7, name: 'David Marquetty', experience: 42500, skill: 35, type: EmployeeTypes.CustomerServiceAgent, image: 'images/avatars/male/1.png', salary: 2200, skills:[0], avatar: 'male1', progress: 0, happiness: 50 },
+    { id: 8, name: 'Simon Karberg', experience: 122500, skill: 30, type: EmployeeTypes.CustomerServiceAgent, image: 'images/avatars/male/2.png', salary: 2200, skills:[0], avatar: 'male1', progress: 0, happiness: 50 },
+    { id: 9, name: 'John Doe', experience: 210000, skill: 55, type: EmployeeTypes.CustomerServiceAgent, image: 'images/avatars/female/1.png', salary: 3000, skills:[0], avatar: 'female1', progress: 0, happiness: 50 },
+    
+    // Project Managers
+    { id: 10, name: 'Marlene	Green', experience: 162500, skill: 40, type: EmployeeTypes.ProjectManager, image: 'images/avatars/female/2.png', salary: 3700, skills:[], avatar: 'female1', progress: 0, happiness: 50 },
+    { id: 11, name: 'Howard Hampton', experience: 70400, skill: 30, type: EmployeeTypes.ProjectManager, image: 'images/avatars/male/3.png', salary: 3000, skills:[], avatar: 'male1', progress: 0, happiness: 50 },
+    { id: 12, name: 'Rosie	Porter', experience: 246400, skill: 50, type: EmployeeTypes.ProjectManager, image: 'images/avatars/female/3.png', salary: 5000, skills:[], avatar: 'female1', progress: 0, happiness: 50 },
+]
