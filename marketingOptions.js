492d101e9a2af68af3bda3c3dc662985e7bdec31
Mads Cordes <Mobilpadde@gmail.com>
2015-11-07 16:29:49 +0100

    Annoying ads
    I'm not sure about the prices nor the conversation rates. Also, introduced an annoyance level, which would probably be somewhat useful for gaining or losing customers.

--- marketingOptions.js
+++ marketingOptions.js
@@ -4,13 +4,55 @@
       name: 'Banner Ads',
       description: 'Shows your banner on various unspecific websites. Impressions are cheaper, but generates fewer new users per impression.',
       pricePerImpression: 0.005,
-      conversationRate: 0.2
+      conversationRate: 0.2,
+      annoyance: 0.4
     },
     {
       id: 2,
       name: 'Targeted Banner Ads',
       description: 'Shows your banner websites related to your product. Impressions are more expensive, but generates more users per impression.',
       pricePerImpression: 0.009,
-      conversationRate: 0.6
-    }  
+      conversationRate: 0.6,
+      annoyance: 0.4
+    },
+    {
+      id: 3,
+      name: 'Mobile Video Ads',
+      description: 'Show a "short elevator pitch"-video ad in mobile games; real annoying but forces the user to see what your company does.',
+      pricePerImpression: 0.012,
+      conversationRate: 0.7,
+      annoyance: 0.7
+    },
+    {
+      id: 4,
+      name: 'Digital Money', // You'd definitely change the name.
+      description: 'Let users earn digital-money in mobile games by forcing them to check out your company web site.',
+      pricePerImpression: 0.012,
+      conversationRate: 0.7,
+      annoyance: 0.5
+    },
+    {
+      id: 5,
+      name: 'Skippable Video Ads', // You'd definitely change the name.
+      description: 'Show a skippable "short elevator pitch"-video ad on multiple video sites, before the video starts',
+      pricePerImpression: 0.020,
+      conversationRate: 0.7,
+      annoyance: 0.5
+    },
+    {
+      id: 6,
+      name: 'Non-Skippable Video Ads', // You'd definitely change the name.
+      description: 'Show a non-skippable "short elevator pitch"-video ad on multiple video sites before the video starts',
+      pricePerImpression: 0.025,
+      conversationRate: 0.7,
+      annoyance: 0.99
+    },
+    {
+      id: 7,
+      name: 'Product Placement', // You'd definitely change the name.
+      description: 'View your products in films and videos',
+      pricePerImpression: 0.030,
+      conversationRate: 0.8,
+      annoyance: 0.05
+    }
 ]
